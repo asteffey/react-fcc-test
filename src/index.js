2c2ab5007bf5aed2486b35c2f985e7116566758d
=Andrew Steffey <asteffey@gmail.com>
2019-10-03 22:09:04 -0400

    Add index.js with useFccTest and FccTests
    `useFccTest` is a React hook to display the FCC tests.
    It can accept two optional parameters, `fccTest` and `queryParam`.
    `fccTest` is used to specify which test should be selected.
    `queryParam` is used to only display the FCC tests if the
    specificed query parameter is present.
    
    `FccTests` is an object which contains a list of the FCC tests.
    A selected item can be passed to `useFccTest` with the `fccTest`
    parameter.

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import 'url-search-params-polyfill';
+import { useEffect } from 'react';
+
+export const FccTests = {
+    tribute_page: 'tribute-page',
+    portfolio: 'portfolio',
+    survey_form: 'survey-form',
+    product_landing_page: 'product-landing-page',
+    technical_docs_page: 'technical-docs-page',
+    random_quote_machine: 'random-quote-machine',
+    markdown_previewer: 'markdown-previewer',
+    drum_machine: 'drum-machine',
+    pomodoro_clock: 'pomodoro-clock',
+    javascript_calculator: 'javascript-calculator',
+    bar_chart: 'bar-chart',
+    scatter_plot: 'scatter-plot',
+    heat_map: 'heat-map',
+    choropleth: 'choropleth',
+    tree_map: 'tree-map'
+};
+
+export const useFccTest = ({ fccTest, queryParam } = {}) => {
+    useEffect(() => {
+        console.log('hasQueryParam(queryParam):' + hasQueryParam(queryParam));
+        if (hasQueryParam(queryParam)) {
+
+            setFccTest(fccTest);
+            const script = document.createElement('script');
+            script.src = 'https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js';
+            script.async = true;
+
+            document.body.appendChild(script);
+
+            return function cleanup() {
+                script.remove();
+            };
+        }
+    }, [fccTest, queryParam]);
+};
+
+const hasQueryParam = (param) => {
+    if (isDefined(param))
+        return new URLSearchParams(window.location.search).get(param) != null;
+    else
+        return true;
+};
+
+const setFccTest = (fccTest) => {
+    if (isDefined(fccTest) && isValidFccTest(fccTest)) {
+        localStorage.setItem('project_selector', fccTest);
+    }
+};
+
+const isDefined = v => typeof v !== 'undefined';
+
+const isValidFccTest = (fccTest) => {
+    return Object.values(FccTests).includes(fccTest);
+};
